f6c6c651f7a830e7605f7d40abcf198da1e8ced0
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-04-16 17:08:11 -0300

    feat(api.js): added url for the other two apis, not needed but handy

--- js/config.js
+++ js/config.js
@@ -1,4 +1,8 @@
 export const URL = "https://darkblue-frog-779608.hostingersite.com";
+export const SAVE_URL =
+  "https://darkblue-frog-779608.hostingersite.com/api/salvar.php";
+export const RANK_URL =
+  "https://darkblue-frog-779608.hostingersite.com/api/ranking.php";
 export let NAMES = [
   "AZIMUTH",
   "BOREAS",
