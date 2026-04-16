219ac4b63b33ea1c3491874ee190fd861f3d53f5
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-04-16 17:11:13 -0300

    feat(modal.js): basic untested ranking builder

--- js/modal.js
+++ js/modal.js
@@ -7,3 +7,22 @@ export default function switchModal() {
     modal.style.display = "flex";
   }
 }
+
+export function displayRanking(ranking) {
+  let rankingTable = querySelector("#ranking");
+  for (let i = 0; i < ranking.length(); i++) {
+    createRankRow(i, ranking[i])
+  }
+}
+
+function createRankRow(index, ranking) {
+  let tr = document.createElement("tr");
+  for (let i = 0; i < array.length; i++) {
+    tr.appendChild((document.createElement("th").textContent = index));
+    tr.appendChild((document.createElement("th").textContent = ranking.nome));
+    tr.appendChild((document.createElement("th").textContent = ranking.tentativas));
+    tr.appendChild((document.createElement("th").textContent = ranking.tempo));
+  }
+
+  return tr;
+}
